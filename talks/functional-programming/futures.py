4f4a291cd3b7a944c340dade472a2fd05abf21ba
James Geddes <jgeddes@turing.ac.uk>
2023-06-20 14:07:01 +0100

    Add acknowledgement

--- talks/functional-programming/futures.py
+++ talks/functional-programming/futures.py
@@ -1,5 +1,6 @@
 import socket
 
+## Based on: https://realpython.com/python-sockets/
 HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
 PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
 
