6eaa0449717543a77306e15dd703b7a36a60a6a8
James Geddes <jgeddes@turing.ac.uk>
2025-01-29 16:58:08 +0000

    Notes

--- notes/images/expression-tree.rkt
+++ notes/images/expression-tree.rkt
@@ -1,31 +1,61 @@
 #lang racket
 
-(require pict)
-(require pict/color)
-(require pict/tree-layout)
+(require pict
+         pict/color
+         pict/tree-layout
+         file/convertible
+         (only-in racket/draw the-color-database)
+         plot)
+
+
+(define col-for-app (send the-color-database find-color "dark gray"))
+(define col-for-lit (send the-color-database find-color "blue"))
+(define col-for-proc (send the-color-database find-color "black"))
+(define col-for-var (send the-color-database find-color "red"))
+
 
 (define (text-node-pict txt col)
   (cc-superimpose
-   (circle 30)
-   (col (text txt))))
+   (disk 30 #:draw-border? #t #:color "white" #:border-color "gray")
+   (text txt (cons col 'modern))))
 
 ;; -> tree-layout?
 (define (lit val)
-  (tree-layout #:pict (text-node-pict (format "~a" val) blue)))
+  (tree-layout #:pict (text-node-pict (format "~a" val) col-for-lit)))
+
+(define (var val)
+  (tree-layout #:pict (text-node-pict (symbol->string val) col-for-var)))
 
 (define (app xs)
   (apply tree-layout
-         (tree-layout #:pict (text-node-pict (format "~a" (car xs)) blue))
-         (map expr (cdr xs))
-         #:pict (text-node-pict "App" black)))
+         (tree-edge (tree-layout #:pict (text-node-pict (format "~a" (car xs)) col-for-proc))
+                    #:edge-color "gray")
+         (map (λ (e) (tree-edge (expr e) #:edge-color "gray")) (cdr xs))
+         #:pict (text-node-pict "App" col-for-app)))
 
 ;; -> tree-layout?
+
 (define (expr e)
   (cond
-    [(pair? e) (app e)]
-    [else      (lit e)]))
+    [(pair? e)   (app e)]
+    [(symbol? e) (var e)]
+    [else        (lit e)]))
 
-(naive-layered 
- (expr '(exp (- (/ (expt x 2) 2))))
- #:x-spacing 5)
 
+(with-output-to-file "tree.pdf"
+  (thunk
+   (write-bytes
+    (convert 
+     (naive-layered 
+      (expr '(exp (- (/ (** x 2) 2))))
+      #:x-spacing 5)
+     'pdf-bytes))
+   )
+  #:exists 'replace)
+
+
+(define (gaussian x)
+  (exp (- (/ (expt x 2) 2))))
+
+(plot-file (function gaussian -3 3 #:y-min 0 #:label "y = gaussian(x)")
+           "gaussian-plot.pdf")
