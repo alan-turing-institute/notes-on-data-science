611392fd2fe282026ee0f4bde92abcf1a5d26501
James Geddes <geddes.james@me.com>
2025-11-08 11:28:29 +0000

    Edits

--- talks/immutable-types/types_talk.ml
+++ talks/immutable-types/types_talk.ml
@@ -1,84 +1,87 @@
 (* Pay no attention to the man behind the curtain *)
-
 let is_zero = ((=) 0) ;;
+
 
 (** I ----------------------------------------------------------
 
-    A philosophy of programming:
+    A philosophy of programming
     
     1. To program is to describe a computation.
     2. To compute is to calculate the value of a function.
     3. Functions turn inputs into outputs.
 
-    For example: the "square" program:
-
-    ( 3 ) ----[ square ]---> ( 9 )
-
-    
-    
-    
  *)
+
+
+
+
 
 
 (** II ---------------------------------------------------------
 
-    Values are the things that are inputs to, and outputs from,
-    functions.
+    Next up: Values
 
-    Every value is a value of some type. Most languages come with some
-    "built-in" types and some way of writing values of those types.
-
-    - [3] is a value of type [int]
-    - [true] and [false] are the only values of type [bool]
-    - ["hello"] is a value of type [string]
-
-    Values can be given names, using the special syntax, let
-    
  *)
- 
+
+
+
+
+
+
+
+
+
 
 (** III --------------------------------------------------------
     
-    Languages also come with built-in functions that operate on those
-    built-in types.
+    What we know so far:
 
-    Every function takes exactly one input and produces exactly one
-    output. 
-    
-    [succ : int -> int] 
-    [pred : int -> int]
-    [is_zero : int -> bool]
-    [string_of_int : int -> string]
+      Built-in types:
+      - 1, 3, 4, ...          : int
+      - true, false           : bool
+      - "hello", "ocaml", ... : string
 
-    [solve_advent_of_code : string -> string] <-- We want to create this!
-        
+      Naming things: [let x = 10]
+
+    Next up: Built-in functions
+      
  *)
-
 
 (** IV ---------------------------------------------------------
 
-    Program 1
-    
-    To "program" all we need to do is to combine the built-in
-    functions to make new functions.
+    What we know so far:
 
-    ...
+      - Built-in types:     [int], [bool], [string].
+      - Built-in functions: [succ : int -> int],
+                            [string_of_int : string -> int], ...
+      - Naming things       [let f x = ...]
+                            
+    Next up: Making new types
 
  *)
+
 
 
 (** V ----------------------------------------------------------
 
-    Making new types. 
+    What we know so far:
+      - Built-in types and functions
+        - [int], [bool], [string].
+        - [succ : int -> int], ...
+      - Naming things
+        - [let f x = ...]
+      - Product types:
+        - [(1, 2) : int * int]
+        - [() : unit]
+        - [fst], [snd]  
 
-    - Types that are isomorphic to (but not identical to!) an existing
-    type
-
-    - Product types
-
-    - Sum types
+        Next up: 
     
+        
+
+
+
  *)
 
-type port = Port of int
+
 
